d8a2a37b59ce42ce49d47dbac8be63de02bb52be
Victoria1349 <grimbug13@gmail.com>
2020-05-12 17:21:56 +0400

    Functions' interfaces

---
+++ DataProfiling.py
@@ -0,0 +1,132 @@
+# Python 3
+import pandas as pd
+
+class DataProfiling(pd.DataFrame):
+    data = pd.DataFrame()
+
+    def __init__(self, dt):
+        """Constructor"""
+        data = dt
+
+
+class Profiling(object):
+
+    def dataType(col):
+        str = "Hello world"
+        return str
+
+    def funcType(func):
+        str = "Hello world"
+        return str
+
+    def findMistakes(data):
+        resData = pd.DataFrame()
+        return resData
+
+    def numbOfRepetitionsOfOneValueInColumn(data):
+        numbs = [0,0,0]
+        return numbs
+
+    def dataStandardization(col):
+        resCol = pd.Series()
+        return resCol
+
+
+class Cleaning(object):
+
+    def findSkips(data):
+        ids = [0, 0, 0]
+        return ids
+
+    def cleanSkips(data):
+        resData = pd.DataFrame()
+        return resData
+
+    def findEjections(data):
+        ids = [0, 0, 0]
+        return ids
+
+    def cleanEjections(data):
+        resData = pd.DataFrame()
+        return resData
+
+    def findNulls(data):
+        ids = [0,0,0]
+        return ids
+
+    def numbOfSkips(data):
+        numb = 0
+        return numb
+
+    def findMissingData(data):
+        ids = [0,0,0]
+        return ids
+
+    def numbOfSkipDataInColumn(col):
+        numb = 0
+        return numb
+
+    def delDuplicates(data):
+        resData = pd.DataFrame()
+        return resData
+
+
+
+class Statistics(object):
+
+    def distributionFunc(data):
+        str = "Hello world"
+        return str
+
+    def frequencyFunc(data):
+        ids = [0.0, 0.0, 0.0]
+        return ids
+
+    def moda(col):
+        moda = 0.0
+        return moda
+
+    def maxValue(col):
+        maxValue = 0.0
+        return maxValue
+
+    def minValue(col):
+        minValue = 0.0
+        return minValue
+
+    def meanValue(col):
+        meanValue = 0.0
+        return meanValue
+
+    def median(col):
+        median = 0.0
+        return median
+
+
+
+class Structures(object):
+
+    def structureDetection(data):
+        str = "Hello world"
+
+    def relationsDetection(data):
+        str = "Hello world"
+
+
+
+class Vizual(object):
+
+    def datasetVisualization(data):
+        str = "Hello world"
+
+
+class Report(object):
+
+    def metadataReport(data):
+        str = "Hello world"
+
+
+
+
+
+
