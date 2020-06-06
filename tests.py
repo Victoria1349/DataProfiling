20dce967757e78316c718b3953bc8bc30cc16c33
Victoria1349 <grimbug13@gmail.com>
2020-06-06 23:51:08 +0400

    tests for maxValue

--- tests.py
+++ tests.py
@@ -11,19 +11,77 @@ from DataProfiling import DataProfiling
 
 class maxValueTests(unittest.TestCase):
 
-    def test_correct(self):
+    def test_diff(self):
         DP = DataProfiling()
-
-        ser = pd.Series([-200, 0, 24, np.nan, 150, 62, 24], ['a', 'b', 'c', 'd', 'e', 'f', 'j'])
+        ser = pd.Series([-200, 0, 2, np.nan, 150, 62, 24], ['a', 'b', 'c', 'd', 'e', 'f', 'j'])
         DP.__setSeries__(ser)
-
-        d = {"price": [1, 2, 0, 4, 5], "count": [0, 4, 0, 3, 1], "percent": [24, 51, 0, 0, 4]}
-        df = pd.DataFrame(d)
-        DP.__setDF__(df)
 
         result = DP.maxValue()
         self.assertEqual(result, 150)
 
+    def test_someSame(self):
+        DP = DataProfiling()
+        ser = pd.Series([-200, 0, 24, np.nan, 150, 0, 24], ['a', 'b', 'c', 'd', 'e', 'f', 'j'])
+        DP.__setSeries__(ser)
+
+        result = DP.maxValue()
+        self.assertEqual(result, 150)
+
+    def test_allSame(self):
+        DP = DataProfiling()
+        ser = pd.Series([150, 150, 150, 150, 150, 150, 150], ['a', 'b', 'c', 'd', 'e', 'f', 'j'])
+        DP.__setSeries__(ser)
+
+        result = DP.maxValue()
+        self.assertEqual(result, 150)
+
+    def test_empty(self):
+        DP = DataProfiling()
+        ser = pd.Series([], [])
+        DP.__setSeries__(ser)
+
+        res = DP.maxValue()
+        if str(res) == str(np.nan): # hack of checking empty data
+            result = True
+        else:
+            result = False
+        self.assertEqual(result, True)
+
+    def test_nulls(self):
+        DP = DataProfiling()
+        ser = pd.Series([0,0,0,0])
+        DP.__setSeries__(ser)
+
+        res = DP.maxValue()
+        if str(res) == str(np.nan): # hack of checking empty data
+            result = True
+        else:
+            result = False
+        self.assertEqual(result, True)
+
+    def test_nans(self):
+        DP = DataProfiling()
+        ser = pd.Series([np.nan,np.nan,np.nan,np.nan])
+        DP.__setSeries__(ser)
+
+        res = DP.maxValue()
+        if str(res) == str(np.nan): # hack of checking empty data
+            result = True
+        else:
+            result = False
+        self.assertEqual(result, True)
+
 
 if __name__ == '__main__':
     unittest.main()
+
+
+''' example
+DP = DataProfiling(
+ser = pd.Series([-200, 0, 24, np.nan, 150, 62, 24], ['a', 'b', 'c', 'd', 'e', 'f', 'j'])
+DP.__setSeries__(ser
+d = {"price": [1, 2, 0, 4, 5], "count": [0, 4, 0, 3, 1], "percent": [24, 51, 0, 0, 4]}
+df = pd.DataFrame(d)
+DP.__setDF__(df
+result = DP.maxValue()
+self.assertEqual(result, 150)'''
